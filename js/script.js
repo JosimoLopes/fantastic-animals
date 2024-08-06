5da171df02fff6a8f707ef23323deed7f7f25e4c
Josimo Lopes <joe.lopes@outlook.com>
2024-08-06 14:00:00 -0400

    refact fetch animals

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ import Tooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initBusinessHours from "./modules/business-hours.js";
-import initFetchAnimals from "./modules/fetch-animals.js";
+import fetchAnimals from "./modules/fetch-animals.js";
 import initFetchBc from "./modules/fetch-bc.js";
 
 const subtleScroll = new SubtleScroll('a[href^="#"]');
@@ -29,5 +29,5 @@ initScrollAnimation();
 initDropdownMenu();
 initMenuMobile();
 initBusinessHours();
-initFetchAnimals();
 initFetchBc();
+fetchAnimals("./animals-api.json", ".numbers-grid");
